46c9fc44616688ae77ed9cf6f8c81b3ccd76c9d5
neominal.solutions <neominal.solutions@gmail.com>
2025-06-17 16:02:58 +0300

    UseState Hooks

--- src/components/user.func.component.tsx
+++ src/components/user.func.component.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Componente ait Propslar export edilen bir tip değildir. Component export edilir. Propslara component üzerinden zaten erişebiliriz
 // { id }: Props -> Object Deconstruction işlemi
@@ -15,6 +15,23 @@ type State = {
 function UserFunctionComponent({ id }: Props) {
 	console.log('id-props', id);
 	const [state, setState] = useState<State>({ users: [] }); //constructor -> this.state = {users:[]}
+
+	// component doma girdiği ve domadan çıktığını anı yakalamamızı sağlayan bir hook. Aynı zamanda api gibi scoket gibi external kaynaklar ile componenti bağlamak içinde kullanırız.
+	// componentDidMount,componentDidUpdate, componentWillUnmount hepsini destekler.
+	useEffect(() => {
+		// asenkron load işlemlerini burada gerçekleştiririz.
+
+		console.log('componentDidMount gibi davranır');
+
+		// 1. yöntem
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setState({ users: data });
+			});
+	}, []); // [] empty dependecy ile sayfa açılışında 1 kereye mahsus tetiklenir. [state1,state2,state3] -> bu kısım ile de state takibi yapıyoruz. [] sadece component doma girdiği ilk anda çalışıyor.
 
 	const addUser = () => {
 		const id = state.users.length + 1;
